ce486a5cd55a0f31f0581f078396fb6f78023b47
Steven Chen <steven2381824724@gmail.com>
2025-09-05 16:31:18 -0400

    fixed bug

--- mipcandy_bundles/unet/unet.py
+++ mipcandy_bundles/unet/unet.py
@@ -116,4 +116,4 @@ if __name__ == "__main__":
 
     model = UNet(in_ch=3, num_classes=1, bilinear=False, features=[32, 64, 128, 256, 512, 512, 512, 512])
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    sanity_check(model=model, input_shape=(1, 1, 244, 244), device=device)
+    sanity_check(model=model, input_shape=(1, 3, 256, 256), device=device)
